a0da8d3fc11a48421c0554071948379bd7b1e313
keyrane <messankeyrane@gmail.com>
2023-06-19 19:15:05 +0200

    finished dark mode

--- src/app/components/DarkModeSwitch.tsx
+++ src/app/components/DarkModeSwitch.tsx
@@ -3,17 +3,22 @@
 import {MdLightMode} from 'react-icons/md'
 import {BsFillMoonFill} from 'react-icons/bs'
 import {useTheme} from 'next-themes'
+import { useEffect, useState } from 'react';
 export default function DarkModeSwitch() {
-
+  
   const {systemTheme , theme, setTheme} = useTheme();
+  const [ mounted, setMounted ] = useState(false);
+
+  useEffect(()=> setMounted(true),[]);
+
   const currentTheme = theme === 'system' ? systemTheme : theme;
   return (
-    <>{currentTheme === 'dark' ? (
+    <>{mounted && (currentTheme === 'dark' ? (
 
-        <MdLightMode onClick={()=> setTheme("light")}/>
+        <MdLightMode className='text-xl cursor-pointer hover:text-amber-400' onClick={()=> setTheme("light")}/>
     ) : (
-        <BsFillMoonFill onClick={()=> setTheme("dark")}/>
-    )}
+        <BsFillMoonFill className='text-xl cursor-pointer hover:text-amber-400' onClick={()=> setTheme("dark")}/>
+    ))}
    </>
     
   )
